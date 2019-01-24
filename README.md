cf8b86b03158eebd281b5f2dd9d00cfd0d92c32f
pakichu <smolguppy@gmail.com>
2019-01-24 16:22:34 -0600

    New IMG and Readme Change

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ RathAPI is a simple rest api that is used to multiply and divide numbers by usin
 3. Hit enter
 ### Multiplying
 1. Start the python program
-2. Go to http://localhost:8000/multiply/"multiplier"/"multiplier" (for local servers)
+2. Go to http://localhost:8000/"multiplier"*"multiplier" (for local servers)
 3. Hit enter
 
 #### Examples
--- rest.py
+++ rest.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/multiply/<int:num>/<int:mult>', methods=['GET'])
+@app.route('/<int:num>*<int:mult>', methods=['GET'])
 def multiply(num, mult):
     return jsonify({'result': num*mult})
-@app.route('/divide/<int:num>/<int:div>', methods=['GET'])
+@app.route('/<int:num>/<int:div>', methods=['GET'])
 def divide(num, div):
     return jsonify({'result': num/div})
 
