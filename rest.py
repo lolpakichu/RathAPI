698e30f2ce94ec63c3d7dcfb5715ae17a0daf9dd
pakichu <smolguppy@gmail.com>
2019-01-23 20:25:13 -0600

    First Commit!

---
+++ rest.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/multiply/<int:num>/<int:mult>', methods=['GET'])
+def multiply(num, mult):
+    return jsonify({'result': num*mult})
+@app.route('/divide/<int:num>/<int:div>', methods=['GET'])
+def divide(num, div):
+    return jsonify({'result': num/div})
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
